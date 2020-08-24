f36af23091cee06607937db0e6c05e1973b4dcac
Prasad2083 <Varavam2083@gmail.com>
2020-08-24 00:29:09 -0400

    Added Json parser utiliy to the project

--- restapi/src/main/java/com/qa/basetestapi/BaseTest.java
+++ restapi/src/main/java/com/qa/basetestapi/BaseTest.java
@@ -8,7 +8,13 @@ public class BaseTest {
 	
 	public Properties prop;
 	
-	
+	public int Response_200=200;
+	public int Response_404=404;
+	public int Response_201=201;
+	public int Response_400=400;
+	public int Response_203=203;
+	public int Response_501=501;
+
 		public BaseTest() throws IOException {
 			
 			prop= new Properties();
--- restapi/src/test/java/com/qa/testapi/TestApiClass.java
+++ restapi/src/test/java/com/qa/testapi/TestApiClass.java
@@ -1,21 +1,33 @@
 package com.qa.testapi;
 
-import java.io.IOException;
-import java.util.Properties;
+import static org.testng.Assert.assertEquals;
 
+import java.io.IOException;
+import java.util.HashMap;
+
+import org.apache.http.Header;
 import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.util.EntityUtils;
+import org.json.JSONObject;
+import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import com.qa.basetestapi.BaseTest;
 import com.qa.client.HttpClient;
+import com.qa.testutilsapi.TestUtils;
 
 public class TestApiClass extends BaseTest {
 	String uri;
 	String url;
 	String serviceurl;
 	BaseTest base;
-	
+	HttpClient clienttest;
+	CloseableHttpResponse httpresponse;
+	Integer statuscode;
+	String response;
+	JSONObject jsonobject;
 	public TestApiClass() throws IOException {
 		
 		super();
@@ -26,17 +38,86 @@ public class TestApiClass extends BaseTest {
 	@BeforeMethod
 	public void setUp() throws IOException{
 		 base= new BaseTest();
+		 clienttest= new HttpClient();
 		url= prop.getProperty("URL");
 		serviceurl=prop.getProperty("ServiceURL");
-		 uri= url+serviceurl;
+		 uri= url+serviceurl; 
 		
 	}
 	
 	@Test
 	public void geturl() throws ClientProtocolException, IOException {
-		HttpClient clienttest= new HttpClient();
-		clienttest.get(uri);
+		
+		
+		    httpresponse=clienttest.get(uri);
+		
+		    statuscode =httpresponse.getStatusLine().getStatusCode(); // To get a response code for particular url request
+			System.out.println(statuscode);
+		
+			response=EntityUtils.toString(httpresponse.getEntity()); // to get the entire response we are using EntityUtils
+			                                                                 //responsestored(httpresponse).get
+			
+			System.out.println(response);
+			
+			
+		    jsonobject = new JSONObject(response);// we are getting response in string format convert to json
+			
+			
+			System.out.println(jsonobject);
+			
+			// Single Value fetching from Json
+			// Can fetch values from json any single value from json
+			
+			String jsonvalue=TestUtils.getjsonvalue(jsonobject, "/per_page");
+			
+			System.out.println("Value of Json Path-----------"+jsonvalue);
+			
+			Assert.assertEquals(Integer.parseInt(jsonvalue), 6,"Value not found");
+			
+			// Array type fetching from Json
+			// Fetching json values from array type data in json
+			
+			String jsonarrayvalue= TestUtils.getjsonvalue(jsonobject, "/data[2]/email");
+			System.out.println("Value of jasonarray"+jsonarrayvalue);
+			Header[] headers=httpresponse.getAllHeaders(); // to validate headers in the response we use this method
+			
+			HashMap<String, String> allheaders = new HashMap<String, String>(); // Store all the headers in hashmap
+			// We need to use hashmap because the headers have key value pairs....Storing everything in key value format
+			
+			for(Header responseheaders:headers) { // for each loop--- is used
+				
+				allheaders.put(responseheaders.getName(), responseheaders.getValue());
+				
+			}
+			
+			System.out.println("Hashmapheaders size---------"+ allheaders);
+			httpresponse.close();
+		}
+		
+	
+	
+	@Test
+	public void statuscode() {
+		assertEquals(String.valueOf(statuscode), String.valueOf(Response_200),"Status Code is not matched");
+		
 	}
+	
+	
+	@Test
+	public void wrongurlvalidate() throws ClientProtocolException, IOException {
+		String urix=uri+"22222222222222";
+		clienttest.get(urix);
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	
 	
 
