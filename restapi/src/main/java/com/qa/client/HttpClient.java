8a92a3ad62c14042dd28fc27db31dace5a1f2290
Prasad2083 <Varavam2083@gmail.com>
2020-08-24 16:14:05 -0400

    Implemented code to automate api with headers using GET method

--- restapi/src/main/java/com/qa/client/HttpClient.java
+++ restapi/src/main/java/com/qa/client/HttpClient.java
@@ -1,6 +1,9 @@
 package com.qa.client;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
@@ -15,13 +18,33 @@ public class HttpClient extends BaseTest {
 		
 	}
 	
-	//Get Method
+	//Get Method without headers
 	public CloseableHttpResponse get(String url) throws ClientProtocolException, IOException {
 		CloseableHttpClient httpclient= HttpClients.createDefault(); // create the connection to send request and response 
 		HttpGet httpget=new HttpGet(url); // Creating method to get request response
 		CloseableHttpResponse httpresponse= httpclient.execute(httpget); // hit the url to get the response
 return httpresponse;
 }
+	
+	//Get Method with headers
+	
+	public CloseableHttpResponse get(String url,HashMap<String,String>hashmapheaders) throws ClientProtocolException, IOException {
+		
+		CloseableHttpClient httpClient = HttpClients.createDefault();
+		HttpGet httpget = new HttpGet(url);
+		
+		for(Map.Entry<String, String> headmap: hashmapheaders.entrySet()) {
+		
+	     httpget.addHeader(headmap.getKey(), headmap.getValue());
+	     
+	     System.out.println("Value from java class hashmap :"+headmap.getKey()+ " "+ headmap.getValue());
+		
+		}
+		CloseableHttpResponse httpresponse= httpClient.execute(httpget);
+		
+		
+		return httpresponse;
+	}
 	
 	
 }
--- restapi/src/test/java/com/qa/testapi/TestApiClass.java
+++ restapi/src/test/java/com/qa/testapi/TestApiClass.java
@@ -78,7 +78,11 @@ public class TestApiClass extends BaseTest {
 			// Fetching json values from array type data in json
 			
 			String jsonarrayvalue= TestUtils.getjsonvalue(jsonobject, "/data[2]/email");
-			System.out.println("Value of jasonarray"+jsonarrayvalue);
+			String jsonarrayvalue1= TestUtils.getjsonvalue(jsonobject, "/data[2]/id");
+			String jsonarrayvalue2= TestUtils.getjsonvalue(jsonobject, "/data[2]/first_name");
+			String jsonarrayvalue3= TestUtils.getjsonvalue(jsonobject, "/data[2]/last_name");
+			System.out.println("Value of jasonarray"+" "+jsonarrayvalue+
+					" "+jsonarrayvalue1+" "+jsonarrayvalue2+" "+jsonarrayvalue3);
 			Header[] headers=httpresponse.getAllHeaders(); // to validate headers in the response we use this method
 			
 			HashMap<String, String> allheaders = new HashMap<String, String>(); // Store all the headers in hashmap
@@ -89,13 +93,86 @@ public class TestApiClass extends BaseTest {
 				allheaders.put(responseheaders.getName(), responseheaders.getValue());
 				
 			}
-			
+			//To get the value of the key in headers and added assertion to validate
+			//System.out.println(allheaders.get("Transfer-Encoding"));
 			System.out.println("Hashmapheaders size---------"+ allheaders);
+			String header1=allheaders.get("Transfer-Encoding");
+			Assert.assertEquals(header1, "chunked","Value not found");
 			httpresponse.close();
 		}
+	
+	
+	
+	
+	
+	@Test
+	public void geturlwithheader() throws ClientProtocolException, IOException {
 		
+		HashMap< String, String> headerhash= new HashMap<String, String>();
+		
+	    	headerhash.put("Connection", "keep-alive");
+	    	headerhash.put("User-Agent", "somekey");
+		
+		    httpresponse=clienttest.get(uri,headerhash);
+		
+		    statuscode =httpresponse.getStatusLine().getStatusCode(); // To get a response code for particular url request
+			System.out.println(statuscode);
+		
+			response=EntityUtils.toString(httpresponse.getEntity()); // to get the entire response we are using EntityUtils
+			                                                                 //responsestored(httpresponse).get
+			
+			System.out.println(response);
+			
+			
+		    jsonobject = new JSONObject(response);// we are getting response in string format convert to json
+			
+			
+			System.out.println(jsonobject);
+			
+			// Single Value fetching from Json
+			// Can fetch values from json any single value from json
+			
+			String jsonvalue=TestUtils.getjsonvalue(jsonobject, "/per_page");
+			
+			System.out.println("Value of Json Path-----------"+jsonvalue);
+			
+			Assert.assertEquals(Integer.parseInt(jsonvalue), 6,"Value not found");
+			
+			// Array type fetching from Json
+			// Fetching json values from array type data in json
+			
+			String jsonarrayvalue= TestUtils.getjsonvalue(jsonobject, "/data[2]/email");
+			String jsonarrayvalue1= TestUtils.getjsonvalue(jsonobject, "/data[2]/id");
+			String jsonarrayvalue2= TestUtils.getjsonvalue(jsonobject, "/data[2]/first_name");
+			String jsonarrayvalue3= TestUtils.getjsonvalue(jsonobject, "/data[2]/last_name");
+			System.out.println("Value of jasonarray"+" "+jsonarrayvalue+
+					" "+jsonarrayvalue1+" "+jsonarrayvalue2+" "+jsonarrayvalue3);
+			Header[] headers=httpresponse.getAllHeaders(); // to validate headers in the response we use this method
+			
+			HashMap<String, String> allheaders = new HashMap<String, String>(); // Store all the headers in hashmap
+			// We need to use hashmap because the headers have key value pairs....Storing everything in key value format
+			
+			for(Header responseheaders:headers) { // for each loop--- is used
+				
+				allheaders.put(responseheaders.getName(), responseheaders.getValue());
+				
+			}
+			//To get the value of the key in headers and added assertion to validate
+			//System.out.println(allheaders.get("Transfer-Encoding"));
+			System.out.println("Hashmapheaders size---------"+ allheaders);
+			String header1=allheaders.get("Transfer-Encoding");
+			Assert.assertEquals(header1, "chunked","Value not found");
+			httpresponse.close();
+		}
 	
 	
+	
+	
+	
+	
+	
+	
+		
 	@Test
 	public void statuscode() {
 		assertEquals(String.valueOf(statuscode), String.valueOf(Response_200),"Status Code is not matched");
